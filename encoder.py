970ff10470d769ef48f8e68446642dc09172b7e1
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-01-26 15:48:13 -0500

    chore: Rename script from bpe.py to encoder.py
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- encoder.py
+++ encoder.py
@@ -1,6 +1,9 @@
 """
 bpe.py - Byte Pair Encoding for building tokenization models
 
+<one line to give the program's name and a brief idea of what it does.>
+Copyright (C) 2024 Austin Berrio
+
 Original Paper: https://arxiv.org/abs/1209.1045
 Referenced Paper: https://paperswithcode.com/method/bpe
 Blog Tutorial: https://leimao.github.io/blog/Byte-Pair-Encoding/
