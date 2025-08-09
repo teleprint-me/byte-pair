1ca07c7db63f850998c7be74ba4d15c011416f7b
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-08-09 18:41:21 -0400

    examples(chore): Remove defunct examples and use the original from the paper

---
+++ examples/bpe.py
@@ -0,0 +1,59 @@
+"""
+bpe.py - Byte Pair Encoder for building tokenization models
+
+Byte Pair Encoder (BPE) Tokenization for Natural Language Processing
+
+Original Paper: https://arxiv.org/abs/1209.1045
+Referenced Paper: https://paperswithcode.com/method/bpe
+Blog Tutorial: https://leimao.github.io/blog/Byte-Pair-Encoding/
+
+Algorithm 1 Learn BPE operations
+"""
+
+import argparse
+import collections
+import re
+
+
+def get_stats(vocab):
+    pairs = collections.defaultdict(int)
+    for word, freq in vocab.items():
+        symbols = word.split()
+        for i in range(len(symbols) - 1):
+            pairs[symbols[i], symbols[i + 1]] += freq
+    return pairs
+
+
+def merge_vocab(pair, v_in):
+    v_out = {}
+    bigram = re.escape(" ".join(pair))
+    p = re.compile(r"(?<!\S)" + bigram + r"(?!\S)")
+    for word in v_in:
+        w_out = p.sub("".join(pair), word)
+        v_out[w_out] = v_in[word]
+    return v_out
+
+
+vocab = {
+    "l o w </w>": 5,
+    "l o w e r </w>": 2,
+    "n e w e s t </w>": 6,
+    "w i d e s t </w>": 3,
+}
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-m",
+    "--num_merges",
+    type=int,
+    default=10,
+    help="Number of BPE merges (default is 10)",
+)
+args = parser.parse_args()
+
+num_merges = args.num_merges
+for i in range(num_merges):
+    pairs = get_stats(vocab)
+    best = max(pairs, key=pairs.get)
+    vocab = merge_vocab(best, vocab)
+    print(best)
