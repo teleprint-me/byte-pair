0781353e2f599b684852a8343197450e6972e27e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-08-11 16:30:03 -0400

    unicode(refactor): Add experimental mapping
    GPT-2 excludes space because the regex prefixes spaces to mergable tokens.

--- examples/unicode.py
+++ examples/unicode.py
@@ -9,10 +9,16 @@ import json
 # https://github.com/openai/gpt-2/blob/master/src/encoder.py#L9
 def unicode() -> dict[int, str]:
     """Generate a GPT-2 Byte to Unicode map."""
+    offset = 0  # Map non-printable to printable codepoints
     codepoints = []  # Build unicode extensions
-    for char in range(512):  # ASCII + Unicode extension range
-        codepoints.append(char)  # add a codepoint
+    for i in range(256):  # ASCII + Unicode extension range
+        if chr(i).isprintable():
+            codepoints.append(i)  # add a codepoint
+        if not chr(i).isprintable():
+            codepoints.append(i + offset)  # get a unicode byte
+        offset += 1  # increment by 1
     return {cpt: chr(cpt) for cpt in codepoints}
 
 
-print(json.dumps(unicode(), indent=2, ensure_ascii=False))
+mapping = sorted((k, v) for k, v in unicode().items())
+print(json.dumps(mapping, indent=2, ensure_ascii=False))
